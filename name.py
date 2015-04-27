b29833dd6c8dc23a1d491a2fa17e016785f720b7
ChengHsi <curiojustus@gmail.com>
2015-04-27 17:16:36 +0200

    Update name.py

--- name.py
+++ name.py
@@ -6,11 +6,6 @@ Random Chinese Name Generator
 Docstrings: http://www.python.org/dev/peps/pep-0257/
 """
 
-__author__ = ' Cheng-Hsi (curiojustus@gmail.com)'
-__copyright__ = 'Copyright (c) 2014 Cheng-Hsi'
-__license__ = 'New-style BSD'
-__vcs_id__ = '$Id$'
-__version__ = '0.1.0'
 
 forename = '''李 王 張 劉 陳 楊 黃 趙 周 吳 徐 孫 朱 馬 胡 郭 林 何 高 梁 鄭 羅 宋 謝 唐 韓 曹 許 鄧 蕭 馮 曾 程 蔡 彭 潘 袁 於 董 餘 蘇 葉 呂 魏 蔣 田 杜 丁 沈 姜 範 江 傅 鐘 盧 汪 戴 崔 任 陸 廖 姚 方 金 邱 夏 譚 韋 賈 鄒 石 熊 孟 秦 閻 薛 侯 雷 白 龍 段 郝 孔 邵 史 毛 常 萬 顧 賴 武 康 賀 嚴 尹 錢 施 牛 洪 龔'''
 
